00435c05e95de2a0d03cb24a31bb902e9ff68c3a
Yvonne Chan <ychan1352@gmail.com>
2016-02-04 13:13:07 -0500

    started hw

---
+++ 01recursion/Recursion.java
@@ -0,0 +1,11 @@
+public class Recursion{
+    public String name(){
+	return "Chan,Yvonne";
+    }
+
+    public double sqrt(double n){
+	if (n < 0){
+	    return IllegalArgumentException();
+	}
+    }
+}
