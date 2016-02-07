bc65d64d9d3f2dc598190ccf33844577fb7386be
Yvonne Chan <ychan1352@gmail.com>
2016-02-07 02:56:52 -0500

    wrote sqrt function

--- 01recursion/Recursion.java
+++ 01recursion/Recursion.java
@@ -5,7 +5,24 @@ public class Recursion{
 
     public double sqrt(double n){
 	if (n < 0){
-	    return IllegalArgumentException();
+	    throw new IllegalArgumentException();
 	}
+	return sqrtHelp(n, n/2);
     }
+
+    public double sqrtHelp(double n, double guess){
+	if (Math.abs(guess*guess - n)/n < 0.000000001){
+	    return guess;
+	}
+	return sqrtHelp(n,(n/guess + guess)/2);
+    }
+
+    public static void main(String[]args){
+	Recursion test = new Recursion();
+	System.out.println(test.sqrt(100));
+	System.out.println(test.sqrt(24));
+	System.out.println(test.sqrt(94));
+	System.out.println(test.sqrt(-64));
+    }
+    
 }
