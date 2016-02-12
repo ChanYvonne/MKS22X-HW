619d97af1cf93275e0c81991487037adba5fb413
Yvonne Chan <ychan1352@gmail.com>
2016-02-12 12:21:23 -0500

    continued working on it

--- 03knightsTour/KnightBoard.java
+++ 03knightsTour/KnightBoard.java
@@ -10,7 +10,7 @@ public class KnightBoard{
 	board = new int[rows][cols];
 	clear();
     }
-
+    
     public void clear(){
 	for (int x = 0; x < board.length; x++){
 	    for (int i = 0; i < board[x].length; i++){
@@ -18,8 +18,38 @@ public class KnightBoard{
 	    }
 	}
     }
+    
+    public int getRows(){
+	return board.length;
+    }
+    
+    public int getCols(){
+	return board[getRow()-1].length;
+    }
+
+    public boolean isValidMove(int col, int row){
+	return col < getCols() && row < getRows();
+    }
 
     public boolean solve(){
+	return helpSolve(0,0);
+    }
+
+    public boolean helpSolve(int col, int row, int steps){
+	//boolean ans = false;
+	if (steps == getRows()*getCols()){
+	    return true;
+	}else if(!(isValidMove(col,row))){
+	    return false;
+	}else{
+	    for (int x = 0; x < getRows(); x++){
+		for (int i = 0; i < getCols();i++){
+		    if (isValidStep()){
+			board[x][i]++;
+		    }
+		}
+	    }
+	}
 	return false;
     }
     
