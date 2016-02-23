0f585a35066d7343dfe5a631a17c71edb88e1edc
Yvonne Chan <ychan1352@gmail.com>
2016-02-23 01:33:54 -0500

    encountered indexOutofBound errors

--- 03knightsTour/KnightBoard.java
+++ 03knightsTour/KnightBoard.java
@@ -25,11 +25,26 @@ public class KnightBoard{
     }
     
     public boolean isValidMove(int col, int row){
-	return col < cols && row < rows;
+	return col < cols && row < rows && col >= 0 && row >= 0;
     }
 
     public boolean solve(){
-	return helpSolve(0,0,1);
+	for (int x = 0; x < rows; x++){
+	    for (int i = 0; i < cols; i++){
+		if (helpSolve(i,x,1)){
+		    return true;
+		}
+	    }
+	}
+	return false;
+    }
+
+    public boolean addKnight(int row, int col, int step){
+	if (board[row][col] == 0){
+	    board[row][col] = step;
+	    return true;
+	}
+	return false;
     }
 
     public boolean helpSolve(int col, int row, int steps){
@@ -37,39 +52,30 @@ public class KnightBoard{
 	if (steps == rows*cols+1){
 	    return true;
 	}
-	if(!(isValidMove(col,row))){
+	if (!(isValidMove(col,row))){
 	    return false;
 	}
-	System.out.println(toString());
-	if (isValidMove(col+1,row+2) && board[row+2][col+1] == 0){
-	    board[row][col] = steps;
-	    return helpSolve(col+1,row+2,steps+1);
-	}else if (isValidMove(col+2,row+1) && board[row+1][col+2] == 0){
-	    board[row][col] = steps;
-	    return helpSolve(col+2,row+1,steps+1);
-	}else if (isValidMove(col+2,row-1) && board[row-1][col+2] == 0){
-	    board[row][col] = steps;
-	    return helpSolve(col+2,row-1,steps+1);
-	}else if (isValidMove(col+1,row-2) && board[row-2][col+1] == 0){
-	    board[row][col] = steps;
-	    return helpSolve(col+1,row-2,steps+1);
-	}else if (isValidMove(col-1,row-2) && board[row-2][col-1] == 0){
-	    board[row][col] = steps;
-	    return helpSolve(col-1,row-2,steps+1);
-	}else if (isValidMove(col-2,row-1) && board[row-1][col-2] == 0){
-	    board[row][col] = steps;
-	    return helpSolve(col-2,row-1,steps+1);
-	}else if (isValidMove(col-2,row+1) && board[row+1][col-2] == 0){
-	    board[row][col] = steps;
-	    return helpSolve(col-2,row+1,steps+1);
-	}else if (isValidMove(col-1,row+2) && board[row+2][col-1] == 0){
-	    board[row][col] = steps;
-	    return helpSolve(col-1,row+2,steps+1);
-	}else{
-	    return false;
-	}
-    }
+	
+	//System.out.println(toString());
+	//System.out.println(col+" "+ row+"\n");
 
+	if (addKnight(row, col, steps)){
+	    if (helpSolve(col+1,row+2,steps+1) ||
+		helpSolve(col+2,row+1,steps+1) ||
+		helpSolve(col+2,row-1,steps+1) ||
+		helpSolve(col+1,row-2,steps+1) ||
+		helpSolve(col-1,row-2,steps+1) ||
+		helpSolve(col-2,row-1,steps+1) ||
+		helpSolve(col-2,row+1,steps+1) ||
+		helpSolve(col-1,row+2,steps+1)){
+		return true;
+	    }
+	    board[row][col] = 0; //for backtracking
+	    //helpSolve(row,col,steps-1);
+	}
+	return false;
+    }
+    
     public String toString(){
 	String ans = "";
 	for (int x =0; x < board.length;x++){
@@ -97,8 +103,29 @@ public class KnightBoard{
     }
 
     public static void main(String[]args){
-	KnightBoard test = new KnightBoard(3);
-	test.solve();
-	test.printSolution();
+	for (int x = 5; x < 8; x++){
+	    KnightBoard test = new KnightBoard(x);
+	    System.out.println(test.solve());
+	    test.printSolution();
+	}
+	KnightBoard test2 = new KnightBoard(3,5);
+	System.out.println(test2.solve());
+	test2.printSolution();
+	
+	KnightBoard test3 = new KnightBoard(5,6);
+	System.out.println(test3.solve());
+	test3.printSolution();
+
+	KnightBoard test4 = new KnightBoard(3,7);
+	System.out.println(test4.solve());
+	test4.printSolution();
+	
+	KnightBoard test5 = new KnightBoard(3,10);
+	System.out.println(test5.solve());
+	test5.printSolution();
+
+	KnightBoard test6 = new KnightBoard(3);
+	System.out.println(test6.solve());
+	test6.printSolution();
     }
 }
