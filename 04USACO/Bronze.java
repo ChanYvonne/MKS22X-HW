b69cf9ef69e1a59eab4975a37231353fedf41604
Yvonne Chan <ychan1352@gmail.com>
2016-02-23 12:16:47 -0500

    continued working

--- 04USACO/Bronze.java
+++ 04USACO/Bronze.java
@@ -1,4 +1,5 @@
 public Bronze{
+    /*
     private int[][] board;
     private int rows, col, elevation;
     private int[][] instructions;
@@ -15,11 +16,33 @@ public Bronze{
 	}
     }
 
-    public static void main(String[]args){
+    public int solve(){
+	int ans = 0;
+	return ans;
+    }
+    */
+    public static void main(String[]args){\
+	string s = "4 6 22 2
+28 25 20 32 34 36
+27 25 20 20 30 34
+24 20 20 20 20 30
+20 20 14 14 20 20
+1 4 4
+1 1 10";
 	try{
-	    Scanner in = new Scanner(new file("input.in"));
-	}catch{
-	    
+	    //Scanner in = new Scanner(new file("makeLatin.in"));
+	    Scanner in = new Scanner(s));
+	    while (in.hasNext()) {
+		Scanner line = new Scanner(in.nextLine());
+		while (line.hasNext()){
+		    System.out.print(pigLatin(line.next())+" ");
+		}
+		System.out.println();
+	    }
+	    //Bronze test = new Bronze(Integer.parseInt(in.next()),Integer.parseInt(in.next()),
+	    //System.out.println(test.solve()) + "6,Chan,Yvonne";
+	}catch (FileNotFoundException e){
+	    e.printStackTrace();
 	}
     }
     
