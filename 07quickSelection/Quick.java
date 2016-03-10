84ec3cb67180517bdb6852c376437c4e761ef18a
Yvonne Chan <ychan1352@gmail.com>
2016-03-09 22:56:11 -0500

    finally got quicksort working!

--- 07quickSelection/Quick.java
+++ 07quickSelection/Quick.java
@@ -6,39 +6,43 @@ public class Quick{
 	//System.out.println(value);
 	int rightIndex = right;
 	int leftIndex = left+1;
+	swap(data,index,left);
+	while (leftIndex < rightIndex){
+	    if (data[leftIndex] > value){
+		swap(data,leftIndex,rightIndex);
+		rightIndex--;
+	    }else{
+		leftIndex++;
+	    }
+	}
+	if (data[leftIndex] > value){
+	    swap(data,left,leftIndex-1);
+	    index = leftIndex-1;
+	}else{
+	    swap(data,left,leftIndex);
+	    index = leftIndex;
+	}
+	//method of creating new array is hopeless :(
+	/*
 	int[] sorted = new int[data.length];
-	for (int x = 0; x < leftIndex && rightIndex > 0; x++){
+	for (int x = left; x < right; x++){
 	    //for (int x = 0; x < data.length; x++){
 	    if (data[x] < value){
 		sorted[leftIndex] = data[x];
 		leftIndex++;
-	    }
-	    if (data[x] > value){
+	    }else{
 		sorted[rightIndex] = data[x];
 		rightIndex--;
 	    }
 	}
+        sorted[rightIndex] = value;
+	data = new int[sorted.length];
 	for (int i = 0; i < sorted.length;i++){
-	    if (sorted[i] == 0){
-		sorted[i] = value;
+	    if (sorted[i] == value){
 		index = i;
-	    }	    
+	    }
+	    data[i] = sorted[i];
 	}
-	/*
-	  data[index] = data[right/2];
-	  data[right/2] = value;
-	
-	  for (int x = left; x < index; x++){
-	  if (data[x] > value){
-	  int temp = data[x];
-	  data[x] = data[rightIndex];
-	  data[rightIndex] = temp;
-	  rightIndex--;
-	  }
-	  }
-	  value = data[right/2];
-	  data[right/2] = data[index];
-	  data[index] = value;
 	*/
 	//printArray(sorted);
 	return index;
@@ -51,6 +55,11 @@ public class Quick{
 
     //	Quick Select will now give the kth smallest value, so the k corresponds to the index of the array! Save you one subtraction!
 
+    private static void swap(int[] data, int spotA, int spotB){
+	int value = data[spotA];
+	data[spotA] = data[spotB];
+	data[spotB] = value;
+    }
     
     public static int quickselect(int[]data, int k){
 	return quickselect(data,k,0,data.length-1);
@@ -59,43 +68,14 @@ public class Quick{
     // when k = 0 return the smallest.
     // 0 <= k < data.length
 
-    /*
-    private static int partitionSorted(int index,int[] data){
-	int value = data[index];
-	//System.out.println(value);
-	int rightIndex = data.length-1;
-	int leftIndex = 0;
-	int[] sorted = new int[data.length];
-	for (int x = 0; x < data.length && leftIndex < data.length && rightIndex > 0; x++){
-	    //for (int x = 0; x < data.length; x++){
-	    if (data[x] < value){
-		sorted[leftIndex] = data[x];
-		leftIndex++;
-	    }
-	    if (data[x] > value){
-		sorted[rightIndex] = data[x];
-		rightIndex--;
-	    }
-	}
-	for (int i = 0; i < sorted.length;i++){
-	    if (sorted[i] == 0){
-		sorted[i] = value;
-	    }	    
-	}
-	printArray(sorted);
-	return sorted[index];
-    }   
-    */
-
 
     // helper method for quickselect recursive
     private static int quickselect(int[]data, int k, int left, int right){
-	printArray(data);
+	//printArray(data);
 	int index = partition(data, left, right);
-	System.out.println(index);
+	//System.out.println(index);
 	if (index == k){
 	    return data[index];
-	    //return partitionSorted(index,data);
 	}
 	//int[] tempary;
 	if (index < k){
@@ -106,7 +86,7 @@ public class Quick{
 	      }	    
 	      return quickselect(data,k,0,tempary.length-1);
 	    */
-	    return quickselect(data,k,index+1,right);
+	    return quickselect(data,k,index,right);
 	}else{
 	    /*
 	      tempary = new int[index];
@@ -115,7 +95,7 @@ public class Quick{
 	      }
 	      return quickselect(tempary,k,0,tempary.length-1);
 	    */
-	    return quickselect(data,k,left,index-1);
+	    return quickselect(data,k,left,index);
 	}
 	
     }
@@ -141,9 +121,19 @@ public class Quick{
     }
     
     public static void main(String[]args){
-	int[] test = {2,4,24,73,35,87,246,90,83,47,45,71};
-	//System.out.println(partition(test,0,test.length-1));
-	System.out.println(quickselect(test,5)); //47
+	int[] test = {2,4,11,-6,35,8,24,1,9,3,17,5,7,0,-100};
+	/*
+	System.out.println(partition(test,0,test.length-1));
+	System.out.println(partition(test,3,test.length-1));
+	System.out.println(partition(test,0,6));
+	System.out.println(partition(test,2,10));
+	*/
+	System.out.println(quickselect(test,5)); //3
+	System.out.println(quickselect(test,12)); //17
+	System.out.println(quickselect(test,14)); //35
+	System.out.println(quickselect(test,0)); //-100
+	System.out.println(quickselect(test,1)); //-6
+	System.out.println(quickselect(test,7)); //5	
 	
     }
 }
