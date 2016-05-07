ecfbfeb0e4b64fd56199c6ec8cadf68a6845a3cb
Yvonne Chan <ychan1352@gmail.com>
2016-05-07 00:17:58 -0400

    worked on heapify, problems with constructor

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -1,68 +1,101 @@
 import java.util.*;
 @SuppressWarnings("unchecked")
-public class MyHeap<T extends Comparable<T>>
-{
-   private int size;
-   private T[] data;
+public class MyHeap<T extends Comparable<T>>{
+    private int size;
+    private T[] data;
 
-   public MyHeap(){
-       data = new T[size];
-   }
+    public MyHeap(){
+	data = (T[]) new Comparable[size];
+    }
    
-   public MyHeap(T[] array, boolean type){
+    public MyHeap(T[] array){
+	data = (T[]) new Comparable[array.length+1];
+	for (int x = 0; x < array.length;x++){
+	    data[x+1] = array.get(x);
+	}
+	heapify();
+    }
+   
+    private void swap(int a, int b){
+	T temp = data[a];
+	data[a] = data[b];
+	data[b] = temp;
+    }
+
+    private void pushDown(int index){
+	if (data[index*2].compareTo(data[index]) > 0){
+	    swap(index,index*2);
+	}else{
+	    swap(index,index*2+1);
+	}
+    }
+   
+    private void pushUp(int index){
+	if (data[index/2].compareTo(data[index]) < 0){
+	    swap(index,index/2);
+	}
+    }
+
+    private void heapify(){
        
-   }
-   
-   public MyHeap(T[] array){
-       data = new T[array.length+1];
-       for (int x = 0; x < array.length;x++){
-	   data[x+1] = array[x];
-       }
-       heapify();
-   }
+    }
 
-   private void pushDown(int index){
-       
-   }
+    public T delete(){
+	return null;
+    }
+   
+    public void add(T x){
+	if (size == data.length-1){
+	    doubleSize();
+	}
+	int index = 1;
+	while (index*2+1 < data.length){
+	}
+    }
+   
+    private void doubleSize(){
+	size *= 2;
+	T[] temp = (T[])new Comparable[size];
+	for (int x = 0; x < data.length; x++){
+	    temp[x] = data[x];
+	}
+	data = temp;
+    }
+   
+    public String toString(){
+	String ans = "[";
+	for (int x = 0; x < data.length; x++){
+	    if (x == data.length-1){
+		ans+= data[x];
+	    }
+	    ans += data[x] + ", ";
+	}
+	return ans + "]";
+    }
 
-   private void pushUp(int index){
+    /*
+    //do this last
+    public MyHeap(boolean isMax){
    
-   }
+    }
+    public MyHeap(T[] array, boolean isMax){
+    this(array);
+    }
+    */
 
-   private void heapify(){
-       
-   }
-
-   public T delete(){
-   
-   }
-   
-   public void add(T x){
-       if (size == data.length-1){
-	   doubleSize();
-       }
-       
-   }
-   
-   private void doubleSize(){
-       size *= 2;
-       T[] temp = new T[size];
-       for (int x = 0; x < data.length; x++){
-	   temp[x] = data[x];
-       }
-       data = temp;
-   }
-   
-   public String toString(){
-       return ArrayList.toString(data);
-   }
-
-   //do this last
-   public MyHeap(boolean isMax){
-   
-   }
-   public MyHeap(T[] array, boolean isMax){
-       this(array);
-   }
+    public static void main(String[]args){
+	ArrayList<Integer> array = new ArrayList<Integer>();
+	array.add(2);
+	array.add(5);
+	array.add(9);
+	array.add(13);
+	array.add(30);
+	array.add(11);
+	array.add(23);
+	array.add(62);
+	array.add(4);
+	array.add(7);
+	MyHeap<Integer> test = new MyHeap(array);
+    }
 
 }
