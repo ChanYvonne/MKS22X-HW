d848772643fc45f001ef78af89455a2a45fd09bf
Yvonne Chan <ychan1352@gmail.com>
2016-05-08 21:45:59 -0400

    got heapify working

--- 13MyHeap/MyHeap.java
+++ 13MyHeap/MyHeap.java
@@ -5,14 +5,15 @@ public class MyHeap<T extends Comparable<T>>{
     private T[] data;
 
     public MyHeap(){
-	data = (T[]) new Comparable[size];
+	data = (T[]) new Comparable[1];
     }
    
     public MyHeap(T[] array){
 	data = (T[]) new Comparable[array.length+1];
 	for (int x = 0; x < array.length;x++){
-	    data[x+1] = array.get(x);
+	    data[x+1] = array[x];
 	}
+	size = array.length;
 	heapify();
     }
    
@@ -22,22 +23,52 @@ public class MyHeap<T extends Comparable<T>>{
 	data[b] = temp;
     }
 
+    /*pushDown
+      precondition: datas[k]'s children are valid heaps
+      postconditions:-the element at index k has been 
+                     shifted to the correct spot.
+                     -data[k] and is a valid heap
+   */    
     private void pushDown(int index){
-	if (data[index*2].compareTo(data[index]) > 0){
-	    swap(index,index*2);
-	}else{
-	    swap(index,index*2+1);
+	if (index*2+1 < data.length){
+	    if (data[index*2].compareTo(data[index]) < 0){
+		swap(index,index*2);
+		pushDown(index*2);
+	    }
+	    if (data[index*2+1].compareTo(data[index]) < 0){
+		swap(index,index*2+1);
+		pushDown(index*2+1);
+	    }
 	}
     }
-   
+
+    /*pushUp
+      precondition: data is a heap with at most one item
+      out of place (element at k)
+      postconditions:-the element at index k has been 
+                     shifted to the correct spot.
+                     -data is a valid heap
+   */
     private void pushUp(int index){
-	if (data[index/2].compareTo(data[index]) < 0){
-	    swap(index,index/2);
+	if (index/2 > 0){
+	    if (data[index/2].compareTo(data[index]) > 0){
+		swap(index,index/2);
+		pushUp(index/2);
+	    }
 	}
     }
 
     private void heapify(){
-       
+	int index = size/2;
+	while (index > 0){
+	    pushDown(index);
+	    index--;
+	}
+	index = size/2;
+	while (index < size){
+	    pushUp(index);
+	    index++;
+	}
     }
 
     public T delete(){
@@ -45,12 +76,21 @@ public class MyHeap<T extends Comparable<T>>{
     }
    
     public void add(T x){
+	if (size == 0){
+	    data[1] = x;
+	}
 	if (size == data.length-1){
 	    doubleSize();
 	}
 	int index = 1;
 	while (index*2+1 < data.length){
+	    if (x.compareTo(data[index]) > 0){
+		pushDown(index);
+		data[index] = x;
+		
+	    }
 	}
+	size++;
     }
    
     private void doubleSize(){
@@ -67,12 +107,13 @@ public class MyHeap<T extends Comparable<T>>{
 	for (int x = 0; x < data.length; x++){
 	    if (x == data.length-1){
 		ans+= data[x];
+	    }else{
+		ans += data[x] + ", ";
 	    }
-	    ans += data[x] + ", ";
 	}
 	return ans + "]";
     }
-
+    
     /*
     //do this last
     public MyHeap(boolean isMax){
@@ -84,18 +125,9 @@ public class MyHeap<T extends Comparable<T>>{
     */
 
     public static void main(String[]args){
-	ArrayList<Integer> array = new ArrayList<Integer>();
-	array.add(2);
-	array.add(5);
-	array.add(9);
-	array.add(13);
-	array.add(30);
-	array.add(11);
-	array.add(23);
-	array.add(62);
-	array.add(4);
-	array.add(7);
+        Integer[] array = {1,5,24,7,15,3,8,4,10,13,6,30,17,9,28};
 	MyHeap<Integer> test = new MyHeap(array);
+	System.out.println(test);
     }
 
 }
