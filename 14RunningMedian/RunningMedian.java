92bfe25c072485efd9ee5b116cacd400584ae9e0
Yvonne Chan <ychan1352@gmail.com>
2016-05-11 01:52:47 -0400

    I think everything is working yay!

--- 14RunningMedian/RunningMedian.java
+++ 14RunningMedian/RunningMedian.java
@@ -1,14 +1,12 @@
 import java.util.*;
 @SuppressWarnings("unchecked")
-public class RunningMedian<T extends Comparable<T>>{
-    private MyHeap<T> max, min;
-    private T[] data;
+public class RunningMedian{
+    private MyHeap<Integer> max, min;
     
     //Create an empty running median set.
     public RunningMedian(){
-	data = (T[]) new Comparable[10];
-	max = new MyHeap<T>(data,true);
-	min = new MyHeap<T>(data,false);
+	max = new MyHeap<Integer>(true);
+	min = new MyHeap<Integer>();
     }  
 	
     /*
@@ -16,15 +14,15 @@ public class RunningMedian<T extends Comparable<T>>{
       Returns the median value
     */
     public double getMedian(){
-	if (max == null || min == null){
+	if (max.size() == 0 && min.size() == 0){
 	    throw new NoSuchElementException();
 	}
 	if (max.size() == min.size()){
-	    return (max.peek() + min.peek())/2;
+	    return (max.peek() + min.peek())/2.0;
 	}else if (max.size() > min.size()){
-	    return max.peek();
+	    return  max.peek();
 	}else{
-	    return min.peek();
+	    return  min.peek();
 	}
     }
     
@@ -32,11 +30,49 @@ public class RunningMedian<T extends Comparable<T>>{
 	add to the "BigValue" heap otherwise. 
 	balance the two heaps so that their size differ by no more than 1. */   
     public void add(Integer x){
-	
+	if (max.size() == 0){
+	    max.add(x);
+	}else if(min.size() == 0){
+	    min.add(x);
+	}else{
+	    if (x*1.0 < getMedian()){
+		max.add(x);
+	    }else{
+		min.add(x);
+	    }
+	}
+	while (min.size() -  max.size() > 1){
+	    if (min.size() > max.size() + 1){
+		Integer temp = min.delete();
+		max.add(temp);
+	    }
+	    if (min.size() + 1 < max.size()){
+		Integer temp = max.delete();
+		min.add(temp);
+	    }
+	}
     }
     
     public String toString(){
-	String ans = max.toString() + "\n" + "median: " + getMedian();
+	return min.toString() + "\n" + max.toString()+ "\nmedian: " + getMedian();
     }
-	
+
+    public static void main(String args[]){
+	RunningMedian test = new RunningMedian();
+	//System.out.println(test);
+	test.add(5);
+	System.out.println(test);
+	test.add(10);
+	test.add(2);
+	test.add(19);
+	test.add(-4);
+	test.add(0);
+	test.add(11);
+	test.add(13);
+	test.add(24);
+	test.add(50);
+	test.add(32);
+	test.add(49);
+	System.out.println(test);
+    }
 }
